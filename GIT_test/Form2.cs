5e975a26ae435e824af1fa8162e36523990d74f4
Bjørn Vegard Tveraaen <btv@usn.no>
2020-01-22 12:49:38 +0100

    Added a comment for testing of github

--- GIT_test/Form2.cs
+++ GIT_test/Form2.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GIT_test
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-
-
-        // This Is a small edit
-
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
-
     }
 }
